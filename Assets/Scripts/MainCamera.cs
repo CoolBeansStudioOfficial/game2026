db221f263b6475edb1bf3b857774cc8cffe59298
CoolBeansStudioOfficial <134745254+CoolBeansStudioOfficial@users.noreply.github.com>
2026-03-08 19:41:24 -0500

    added parallax background

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -5,14 +5,21 @@ public class MainCamera : MonoBehaviour
     public Transform target;
     public float followSpeed;
 
+    public Transform backgroundImage;
+    public float parallaxMultiplier;
+
     // Update is called once per frame
     void Update()
     {
         if (target == null) return;
 
+        //move camera
         Vector3 newPosition = Vector3.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
         newPosition.z = transform.position.z;
-
         transform.position = newPosition;
+
+        //move background image
+        newPosition = new(transform.position.x * parallaxMultiplier, transform.position.y * parallaxMultiplier, backgroundImage.localPosition.z);
+        backgroundImage.localPosition = newPosition;
     }
 }
